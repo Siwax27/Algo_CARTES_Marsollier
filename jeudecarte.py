281e7681cd7d9a4e77cf8b048b25e7c2610d2680
Kévin Marsollier <93644850+Siwax27@users.noreply.github.com>
2022-05-06 08:59:47 +0200

    implémantation class mage partie 1

--- jeudecarte.py
+++ jeudecarte.py
@@ -1,18 +1,63 @@
-class Carte:
-    def __init__(self,mana,nom,description,attribut):
+class carte:
+    def __init__(self,nom,mana,description,attribut):
         self.mana = mana
         self.name = nom
         self.desc = description
         self.attr = attribut
     
-    def nameCarte(self):
+    def getName(self):
         return self.name
     
-    def manaCarte(self):
+    def getMana(self):
         return self.mana
 
-    def descriptionCarte(self):
+    def getDescription(self):
         return self.desc
 
-    def attributCarte(self):
+    def getAttribut(self):
         return self.attr
+    
+class Mage:
+    def __init__(self,nom,mana,pv,):
+        self.mana = mana
+        self.Hp = pv
+        self.main = [carte("dragon",10,"un puissant dragon qui crache du feu","boule de feu\n"),carte("soldat",5,"un soldat lambda","coup d'épée\n")]
+        self.defausse = 0
+        self.name = nom
+
+    def manaJ(self):
+        self.mana = 100
+        return self.mana
+
+    def defausseCarte(self):
+        self.defausse = self.defausse + 1
+        return self.defausse
+        
+    def carteMain(self):
+        return self.main
+
+    def pvJ(self):
+        return self.Hp
+    
+    def mainJ(self):
+        self.main = 7
+        return self.main
+    
+    def jouerCarte(self):
+        print("vois ci vos carte en main", self.main)
+        propositionJ = int(input("quel carte voulez vous jouer ?\n"))
+        if(propositionJ == "dragon"):
+            print ("vous avez poser la carte dragon sur le jeu")
+            self.mana = self.mana - 10
+        else :
+            print("vous avez poser la carte ",propositionJ, "sur le jeu")
+            self.mana = self.mana - propositionJ
+        
+
+    def play(self):
+        self.carteMain()
+        self.jouerCarte()
+        print("bien joué vous avez poser une carte")
+
+myGame = Mage("John",100,20)
+myGame.play()
